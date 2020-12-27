991b5a212dcddb5977f3aead95babd4b373c8b2b
GrumpycatInHeaven <53279534+GrumpycatInHeaven@users.noreply.github.com>
2020-12-28 02:09:31 +0300

    Update kraska.js

--- kraska.js
+++ kraska.js
@@ -1,10 +1,27 @@
+//Flags
+var img = new SimpleImage("");
+
+for (var pixel of img.values()){
+    if (pixel.getX() < img.getWidth()/3){
+        pixel.setRed(pixel.getRed() + 255);
+    } 
+    else if (pixel.getX() > img.getWidth()/3  && pixel.getX() < 2 * img.getWidth()/3) {
+      pixel.setBlue(pixel.getBlue() + 255);
+    }
+    else if(pixel.getX() >= 2*img.getWidth()/3) {
+      pixel.setGreen(pixel.getGreen() + 255); 
+  }
+}
+print(img);
+
+//Change pixels on another color
 function swapRedGreen(pixel){
     var SBlue = pixel.getBlue();
     pixel.setRed(pixel.getGreen());
     pixel.setGreen(SBlue);
 }
 
-var img = new SimpleImage("x8DKhRPk-Uw.jpg");
+var img = new SimpleImage("");
 print(img);
 for (var pixels of img.values()){
     swapRedGreen(pixels);
